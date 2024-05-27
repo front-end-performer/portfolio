4c321176896013ac5d2e87a11cad2a84f87bbea3
Oleksii <sodolinskyi@gmail.com>
2024-05-27 23:29:53 +0200

    added image

--- components/App/Navbar.vue
+++ components/App/Navbar.vue
@@ -15,17 +15,22 @@
           <div v-if="link.avatar" class="flex items-center gap-4">
             <UTooltip text="Open for network!" class="cursor-pointer">
               <UAvatar
-              :src="link.avatar.src"
-              :size="breakpoints.w <= 667 ? 'sm' : 'lg'"
-              alt="logo"
-            />
-
+                :src="!isHovered ? link.avatar.cartoon : link.avatar.original"
+                :size="breakpoints.w <= 667 ? 'sm' : 'lg'"
+                @mouseover="isHovered = true"
+                @mouseleave="isHovered = !isHovered"
+                alt="logo"
+              />
             </UTooltip>
             <div class="flex flex-col items-start">
-              <h3 class="text-sm sm:text-2xl font-semibold text-slate-700 dark:text-slate-100">
+              <h3
+                class="text-sm sm:text-2xl font-semibold text-slate-700 dark:text-slate-100"
+              >
                 {{ link.label }}
               </h3>
-              <p class="text-xs sm:text-base text-slate-500 dark:text-slate-300">
+              <p
+                class="text-xs sm:text-base text-slate-500 dark:text-slate-300"
+              >
                 {{ $t("globals.subhead") }}
               </p>
             </div>
@@ -46,6 +51,8 @@
 const { locale, defaultLocale } = useI18n();
 const { breakpoints } = useBreakpoints();
 const cookie = useCookie("lang");
+
+const isHovered = ref(false);
 
 const updatedLocale = computed({
   get: () => locale.value,
@@ -68,7 +75,8 @@ const links = [
   {
     label: "Oleksii Sodolinskyi",
     avatar: {
-      src: "https://avatars.githubusercontent.com/u/739974?v=4",
+      original: "/profile_img.png",
+      cartoon: "https://avatars.githubusercontent.com/u/739974?v=4",
     },
   },
   {
